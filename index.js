e9551225c6c6258431cfdc78b7393d79ac5a6528
Julian Elmasry <julian31186@gmail.com>
2022-01-17 13:10:04 -0500

    final changes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-
 //Dice One
 var numberOne =Math.floor(1 + Math.random()*6);
 var randomDiceImage = "dice" + numberOne + ".png";
@@ -10,7 +9,22 @@ image1.setAttribute("src",randomImageSource);
 
 //Dice Two
 var numberTwo =Math.floor(1 + Math.random()*6); 
-var randomDiceImage = "dice" + numberTwo + ".png";
-var randomImageSource = "assets/" + randomDiceImage;
+var randomDiceImage2 = "dice" + numberTwo + ".png";
+var randomImageSource2 = "assets/" + randomDiceImage2;
 var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src",randomImageSource);
+image2.setAttribute("src",randomImageSource2);
+
+
+//Conditionals
+if (numberOne > numberTwo) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!🚩";
+}
+
+else if (numberTwo > numberOne) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+}
+
+else  {
+    document.querySelector("h1").innerHTML = "Draw!🏳️";
+}
+    
